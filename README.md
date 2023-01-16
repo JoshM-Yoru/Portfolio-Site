47a1711e81b02aab90078ab70eb6fe33cb84ebf1
JoshM-Yoru <jmaciel335i@gmail.com>
2023-01-16 12:50:05 -0500

    switched video component to react component, implemented routing logic for showing what route you are in

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ This portfolio is still a work in progress but you are able to visit the site: [
 
 + ~~Set up form to actually send an email~~
 + ~~Set up resume with PDF.js(or possibly link to a google docs)~~
-+ Change embedded videos to be clickable images that will then load the video
++ ~~Change embedded videos to be clickable images that will then load the video~~
++ ~~Implement logic for navbar to show what route you are in~~
 + Stop certain animations from happening on mobile
-+ Implement logic for navbar to show what route you are in
 + Add on scroll animations
 + Code refactor to stick with DRY priniciples
 
---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import YoutubeLogo from "../assets/youtube.png";
+import "../styles/VideoCard.css"
+
+export interface Props {
+    title: string;
+    body: string;
+    src: string;
+    href: string;
+    id: string;
+}
+
+const VideoCard: React.FC<Props> = ({ title, src, body, href, id }) => {
+    const imageLinkPrefix: string = "https://i.imgur.com/";
+
+    const linkedImage = imageLinkPrefix + id;
+
+    const [showVideo, setShowVideo] = useState<boolean>(false);
+
+    const handleVideo = () => {
+        setShowVideo(true);
+    }
+
+    console.log(showVideo);
+
+    return (
+        <div className="link-card" >
+            <h2 className="article-title">
+                {title}
+            </h2>
+            <p className="article-body">
+                {body}
+            </p>
+            <div className="video-container" onClick={handleVideo}>
+                {showVideo ?
+                    <iframe
+                        loading="lazy"
+                        src={href}
+                        title="YouTube video player"
+                        frameBorder={"0"}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen></iframe>
+                    :
+                    <div className="lazy-loading-content image-container">
+                        <div className="preview-wrapper">
+                            <img className="preview-image" src={linkedImage} alt={title} />
+                            <button className="play-container-button"><img className="play-button" src={YoutubeLogo} alt="play" /></button>
+                        </div>
+                    </div>
+                }
+            </div>
+        </div >
+    )
+}
+
+export default VideoCard
